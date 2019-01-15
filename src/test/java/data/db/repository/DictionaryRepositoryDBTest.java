d8e82843da07f2e01a5d5778d2a290e48e90bf1f
vlad <slavayus@gmail.com>
2019-01-15 23:36:57 +0300

    add update test

--- src/test/java/data/db/repository/DictionaryRepositoryDBTest.java
+++ src/test/java/data/db/repository/DictionaryRepositoryDBTest.java
@@ -3,6 +3,7 @@ package data.db.repository;
 import data.db.entity.DictionaryEntity;
 import data.db.model.Dictionary;
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -180,7 +181,69 @@ public class DictionaryRepositoryDBTest {
     }
 
     @Test
-    public void update() {
+    public void updateMissingRecord() {
+        repository.update(new Dictionary("YEE", "YEE"));
+    }
+
+    @Test
+    public void updateExistingRecord() {
+        DictionaryEntity dictionaryEntity = new DictionaryEntity();
+        dictionaryEntity.setWord("YYEE");
+        dictionaryEntity.setDescription("YYEE");
+        session.beginTransaction();
+        session.save(dictionaryEntity);
+        session.getTransaction().commit();
+        session.close();
+
+        repository.update(new Dictionary("YYEE", "YEE"));
+
+        session = HibernateUtil.getSession();
+        session.beginTransaction();
+        Query query = session.createQuery("FROM DictionaryEntity E WHERE E.word = :word");
+        query.setParameter("word", "YYEE");
+        DictionaryEntity singleResult = (DictionaryEntity) query.getSingleResult();
+        session.getTransaction().commit();
+
+        Assert.assertEquals("YYEE", singleResult.getWord());
+        Assert.assertEquals("YEE", singleResult.getDescription());
+    }
+
+    @Test
+    public void updateExistingRecordTheSameValue() {
+        DictionaryEntity dictionaryEntity = new DictionaryEntity();
+        dictionaryEntity.setWord("YYEE");
+        dictionaryEntity.setDescription("YYEE");
+        session.beginTransaction();
+        session.save(dictionaryEntity);
+        session.getTransaction().commit();
+        session.close();
+
+        repository.update(new Dictionary("YYEE", "YYEE"));
+
+        session = HibernateUtil.getSession();
+        session.beginTransaction();
+        Query query = session.createQuery("FROM DictionaryEntity E WHERE E.word = :word");
+        query.setParameter("word", "YYEE");
+        DictionaryEntity singleResult = (DictionaryEntity) query.getSingleResult();
+        session.getTransaction().commit();
+
+        Assert.assertEquals("YYEE", singleResult.getWord());
+        Assert.assertEquals("YYEE", singleResult.getDescription());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void updateNullWord() {
+        repository.update(new Dictionary(null, "YEE"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void updateNullDescription() {
+        repository.update(new Dictionary(null, "YEE"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void updateNullAll() {
+        repository.update(new Dictionary(null, null));
     }
 
     @Test
