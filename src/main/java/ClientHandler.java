667b3dc2df4d3d7f556abfdc8543abd22ee76ab8
vlad <slavayus@gmail.com>
2019-01-15 17:12:08 +0300

    add endpoints for each command

--- src/main/java/ClientHandler.java
+++ src/main/java/ClientHandler.java
@@ -1,4 +1,5 @@
 import com.google.gson.Gson;
+import data.db.repository.DictionaryRepository;
 import data.tcp.model.Request;
 
 import java.io.DataInputStream;
@@ -8,9 +9,11 @@ import java.net.Socket;
 
 public class ClientHandler implements Runnable {
     private final Socket client;
+    private final DictionaryRepository repository;
 
     ClientHandler(Socket client) {
         this.client = client;
+        repository = new DictionaryRepository();
     }
 
     @Override
@@ -20,19 +23,46 @@ public class ClientHandler implements Runnable {
 
             System.out.println("New client attached");
 
+            loop:
             while (!client.isClosed()) {
                 System.out.println("Server reading from channel");
                 String entry = in.readUTF();
                 Request request = new Gson().fromJson(entry, Request.class);
                 System.out.println("READ from clientDialog message - " + entry);
 
-                if (request.getCommand().equalsIgnoreCase("quit")) {
-                    break;
+                switch (request.getCommand()) {
+                    case "quit":
+                        break loop;
+                    case "select":
+                        System.out.println("Server try writing to channel");
+                        out.writeUTF("Server reply - " + new Gson().toJson(repository.select(request.getDictionary())) + " - OK");
+                        out.flush();
+                        break;
+                    case "find":
+                        System.out.println("Server try writing to channel");
+                        out.writeUTF("Server reply - " + new Gson().toJson(repository.find(request.getDictionary())) + " - OK");
+                        out.flush();
+                        break;
+                    case "insert":
+                        System.out.println("Server try writing to channel");
+                        out.writeUTF("Server reply - " + new Gson().toJson(repository.insert(request.getDictionary())) + " - OK");
+                        out.flush();
+                        break;
+                    case "update":
+                        System.out.println("Server try writing to channel");
+                        out.writeUTF("Server reply - " + new Gson().toJson(repository.update(request.getDictionary())) + " - OK");
+                        out.flush();
+                        break;
+                    case "delete":
+                        System.out.println("Server try writing to channel");
+                        out.writeUTF("Server reply - " + new Gson().toJson(repository.delete(request.getDictionary())) + " - OK");
+                        out.flush();
+                        break;
+                    default:
+                        out.writeUTF("Server reply - incorrect command - OK");
+                        out.flush();
                 }
 
-                System.out.println("Server try writing to channel");
-                out.writeUTF("Server reply - " + new Gson().toJson(request) + " - OK");
-                out.flush();
             }
             client.close();
 
@@ -40,7 +70,5 @@ public class ClientHandler implements Runnable {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
     }
 }
--- src/main/java/data/db/repository/DictionaryRepository.java
+++ src/main/java/data/db/repository/DictionaryRepository.java
@@ -2,24 +2,42 @@ package data.db.repository;
 
 import data.db.model.Dictionary;
 
+import java.util.Collections;
+import java.util.List;
+
 public class DictionaryRepository {
-    public void select(Dictionary record) {
-        System.out.println("Getting description");
+    public Dictionary select(Dictionary record) {
+        record = record == null ? new Dictionary() : record;
+        record.setWord("select");
+        record.setDescription("Getting description");
+        return record;
     }
 
-    public void find(Dictionary record) {
-        System.out.println("Searching by mask");
+    public List<Dictionary> find(Dictionary record) {
+        Dictionary dictionary = new Dictionary();
+        dictionary.setWord("find");
+        dictionary.setDescription("Searching by mask");
+        return Collections.singletonList(dictionary);
     }
 
-    public void insert(Dictionary record) {
-        System.out.println("Inserting new record");
+    public Dictionary insert(Dictionary record) {
+        record = new Dictionary();
+        record.setWord("inser");
+        record.setDescription("Inserting new record");
+        return record;
     }
 
-    public void update(Dictionary record) {
-        System.out.println("Updating the record");
+    public Dictionary update(Dictionary record) {
+        record = new Dictionary();
+        record.setWord("update");
+        record.setDescription("Updating the record");
+        return record;
     }
 
-    public void delete(Dictionary record) {
-        System.out.println("Deleting the record");
+    public Dictionary delete(Dictionary record) {
+        record = new Dictionary();
+        record.setWord("delete");
+        record.setDescription("Deleting the record");
+        return record;
     }
 }
